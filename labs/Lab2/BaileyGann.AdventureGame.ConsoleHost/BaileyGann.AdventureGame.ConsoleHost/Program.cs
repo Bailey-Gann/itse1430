f4fb3c77ce89313b047280c36d1990ae4987d751
Bailey Gann <shadowhawk20033@gmail.com>
2022-03-01 21:47:09 -0600

    end of day 3/1/2022

--- labs/Lab2/BaileyGann.AdventureGame.ConsoleHost/BaileyGann.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab2/BaileyGann.AdventureGame.ConsoleHost/BaileyGann.AdventureGame.ConsoleHost/Program.cs
@@ -10,15 +10,114 @@ namespace BaileyGann.AdventureGame.ConsoleHost
 {
     class Program
     {
+        static string[] blank;
         static void Main(string[] args)
         {
-            //Empty code...
+            int mainCalled = 0;
+            if(mainCalled == 0)
+            {
+                var today = DateTime.Now;
+                Console.WriteLine("Bailey Gann \nITSE 1430 \n" + today + "\n");
+            }
+
+            mainCalled++;
+
+            do
+            {
+
+            } while (true);
+        }
+
+        static char DisplayMenu ()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Q)uit\n");
+
+            string input = Console.ReadLine().ToUpper();
+
+            //Validate input
+            //else
+            //{
+            //    Console.WriteLine("Invalid input");
+            //    Console.WriteLine(" ");
+            //    return 'X';
+        };
+            private static void CallToQuit ()
+        {
+            bool answer = Confirm("Are you sure you want to quit? (Y/N)");
+
+            if (answer == false)
+                Main(blank);
+            else
+            {
+                Console.WriteLine("\nGoodbye!!\n");
+                //Terminate Program
+            }
+        }
+        static string GetStringInputList (string[] arrayUsed, string message )
+        {
+            string input = "";
+            bool validateOption = false;
+
+            do
+            {
+                Console.WriteLine(message);
+                input = Console.ReadLine().ToUpper();
+
+                for (int i = 0; i < arrayUsed.Length; i++)
+                {
+                    if(input == arrayUsed[i].ToUpper())
+                    {
+                        input = input.ToUpper();
+                        validateOption = true;
+                        break;
+                    } 
+                }
+                if(validateOption == false)
+                {
+                    Console.WriteLine("Invaild input! \nPlease choose a value from the list...\n");
+                } 
+
+            } while (!validateOption);
+
+            return input;
+        }
+
+        static string GetStringInput_NON_List ( string message )
+        {
+            Console.WriteLine(message);
+
+            return Console.ReadLine();
+        }
+        static bool Confirm ( string message )
+        {
+            Console.Write(message);
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                //Validate 'Y' or 'N'
+
+                if (key.Key == ConsoleKey.Y)
+                {
+                    Console.WriteLine('Y');
+                    return true;
+
+                } else if (key.Key == ConsoleKey.N)
+                {
+                    Console.WriteLine('N');
+                    return false;
+                };
+            } while (true);
         }
     }
 }
 //Story 0:
 //Story 1: Complete
-//Story 2:
+//Story 2: Complete
 //Story 3:
 //Story 4:
 //Story 5:
--- labs/Lab2/BaileyGann.AdventureGame.ConsoleHost/BaileyGann.AdventureGame/Class1.cs
+++ labs/Lab2/BaileyGann.AdventureGame.ConsoleHost/BaileyGann.AdventureGame/Class1.cs
@@ -2,5 +2,51 @@
 
 namespace BaileyGann.AdventureGame
 {
-    
+    class Room
+    {
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private string _name;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private int _id;
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        private string _description;
+    }
+
+    class World
+    {
+        public string[] rooms;
+
+        public string WorldDescription
+        {
+            get { return _wDescription; }
+        }
+        private string _wDescription = "\nWelcome to Hydrexel!\nA land of magic, dragons, war and sometimes peace, many who enter die.\n";
+
+        //TODO: Implement 9 rooms
+        //Room '0' in array is starting room
+    }
+
+    class Player
+    {
+        public int RoomNum
+        {
+            get { return _roomNum; }
+            set { _roomNum = value; }
+        }
+        private int _roomNum;
+    }
 }
